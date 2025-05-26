88b10c6559317934c7f1f8c9542ded85250abbfb
Shvain <takuto3987@gmail.com>
2025-05-26 14:36:01 +0900

    Initial Commit

---
+++ LiveMemo/LiveMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveMemoApp.swift
+//  LiveMemo
+//
+//  Created by Takuto Oyama on 2025/05/26.
+//
+
+import SwiftUI
+
+@main
+struct LiveMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiveMemoTests/LiveMemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiveMemoTests.swift
+//  LiveMemoTests
+//
+//  Created by Takuto Oyama on 2025/05/26.
+//
+
+import Testing
+@testable import LiveMemo
+
+struct LiveMemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
